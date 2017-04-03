93cf2ad3c66695ca5276fbd66ff623c820320425
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2017-04-03 15:29:39 -0600

    First Commit!

---
+++ api/controllers/FestivalController.js
@@ -0,0 +1,11 @@
+/**
+ * FestivalController
+ *
+ * @description :: Server-side logic for managing Festivals
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
